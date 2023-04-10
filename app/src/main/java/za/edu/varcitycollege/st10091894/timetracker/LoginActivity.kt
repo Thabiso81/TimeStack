6d87d70236a985e0938a0bc2083393b333124602
Thabiso <st10091894@vcconnect.edu.za>
2023-04-10 17:00:01 +0000

    Added bottom nav bar in Main Activity layout

--- app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/LoginActivity.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/LoginActivity.kt
@@ -1,11 +1,22 @@
 package za.edu.varcitycollege.st10091894.timetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_main)
+
+        // for testing purposes
+        val nextPage = findViewById<Button>(R.id.login)
+
+        nextPage.setOnClickListener(){
+            val nextPage = Intent(this, MainActivity::class.java)
+            startActivity(nextPage)
+            finish()
+        }
     }
 }
--- app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/MainActivity.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/MainActivity.kt
@@ -2,10 +2,15 @@ package za.edu.varcitycollege.st10091894.timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navBar = findViewById<BottomNavigationView>(R.id.bottomNavMenu)
+        //remove shadow from bottom nav bar when program runs
+        //navBar.background = null
+
     }
 }
