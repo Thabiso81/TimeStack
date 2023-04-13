03c69d2e2869578aaaa24ef8ed3eba481f57b4db
Thabiso <st10091894@vcconnect.edu.za>
2023-04-13 04:33:27 +0000

    Adde three fragments to the project and, Connected the fragment layouts to the bottom navigation view.

--- app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/MainActivity.kt
+++ app/src/main/java/za/edu/varcitycollege/st10091894/timetracker/MainActivity.kt
@@ -2,15 +2,39 @@ package za.edu.varcitycollege.st10091894.timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private val timeSheetEntryFragment = TimeSheetEntryFragment()
+private val timeSheetCategoryFragment = TimeSheetCategoryFragment()
+private val timeSheetStatsFragment = TimeSheetStatsFragment()
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navBar = findViewById<BottomNavigationView>(R.id.bottomNavMenu)
-        //remove shadow from bottom nav bar when program runs
-        //navBar.background = null
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavMenu)
 
+        //default fragment that shows up first when app is first openned
+        replaceFragment(timeSheetEntryFragment)
+
+        //set bottom navigation view functionality
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.miEntries -> replaceFragment(timeSheetEntryFragment)
+                R.id.miCreateCategory -> replaceFragment(timeSheetCategoryFragment)
+                R.id.miGraph -> replaceFragment(timeSheetStatsFragment)
+            }
+            true
+        }
+
+    }
+
+    //Method that switches out fragments on the main activity
+    fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.frameLayout, fragment)
+            transaction.commit()
+        }
     }
 }
